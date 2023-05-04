968d63855ccc251e8a740d7039d0617fd50d53b6
Niall Moore <niallam22@gmail.com>
2023-05-04 05:54:47 +0530

    catch all routing address

--- api/index.js
+++ api/index.js
@@ -28,7 +28,7 @@ app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
 // Catch-all route for serving index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  res.sendFile(path.join(__dirname, "..", 'client/build', 'index.html'));
 });
 
 app.listen(process.env.PORT, ()=>{
